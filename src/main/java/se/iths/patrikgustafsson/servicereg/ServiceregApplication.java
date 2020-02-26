d9dd8559352dedbb776f4d5da5380a88b06a197d
gitPg <pg.patrik@gmail.com>
2020-02-26 13:55:02 +0100

    first commit

---
+++ src/main/java/se/iths/patrikgustafsson/servicereg/ServiceregApplication.java
@@ -0,0 +1,13 @@
+package se.iths.patrikgustafsson.servicereg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceregApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceregApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/patrikgustafsson/servicereg/ServiceregApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.patrikgustafsson.servicereg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceregApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
